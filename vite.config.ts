fccafa6394d5ceae0c64d5c37bd967fa335a68c7
werfsdkjzn <w26532643@gmail.com>
2025-04-05 22:41:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sb1-kzzzj2nb/', // Your GitHub repository name here
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
